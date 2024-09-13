2485ca37e842ce3678b012d8032fb5f856a2b523
Herbert174 <herbertms174@gmail.com>
2024-09-13 15:18:57 -0300

    Ajuste remoção carrinho

--- remover_carrinho.php
+++ remover_carrinho.php
@@ -7,7 +7,7 @@
 	$usuario     = $_SESSION['id_usuario'];
 	$produto_id  = $_GET['produto_id'];
 
-	$sql = " DELETE FROM carrinho_de_compras WHERE id_produto = '$produto_id' ";
+	$sql = " DELETE FROM carrinho_de_compras WHERE id_produto = '$produto_id' AND id = $usuario ";
 
 	$objDb = new database();//Instância a classe
 	$link = $objDb -> conecta_mysql();//Executa função de conexão com o MySQL
